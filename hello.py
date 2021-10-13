9af7def347df110f488540949cb5a02357576a36
LiuMin2021 <min.liu@bulme.at>
2021-10-13 07:42:25 +0100

    asterisks

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+# meine Hello-Programm
+import datetime
 
 print("Guten Start ins neue Schuljahr! ")
 n = input ("Wie is dein Name? ")
 print(f'Guten Tag {n}!')
+
+x = datetime.datetime.now()
+print(x)
 
